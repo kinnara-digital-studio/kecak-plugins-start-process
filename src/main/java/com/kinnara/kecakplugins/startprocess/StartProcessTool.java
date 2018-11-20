7a5a91aa8863c7d5521f77aa845df96537ec39df
aristo_sh <aristo.hadisoeganda@gmail.com>
2018-11-20 14:38:00 +0700

    add appId

--- src/main/java/com/kinnara/kecakplugins/startprocess/StartProcessTool.java
+++ src/main/java/com/kinnara/kecakplugins/startprocess/StartProcessTool.java
@@ -1,19 +1,30 @@
 package com.kinnara.kecakplugins.startprocess;
 
+import org.joget.apps.app.dao.AppDefinitionDao;
 import org.joget.apps.app.model.AppDefinition;
+import org.joget.apps.app.model.PackageDefinition;
 import org.joget.apps.app.service.AppPluginUtil;
+import org.joget.apps.app.service.AppService;
 import org.joget.apps.app.service.AppUtil;
 import org.joget.commons.util.LogUtil;
 import org.joget.plugin.base.DefaultApplicationPlugin;
+import org.joget.plugin.base.PluginWebSupport;
 import org.joget.workflow.model.WorkflowAssignment;
+import org.joget.workflow.model.WorkflowProcess;
 import org.joget.workflow.model.WorkflowProcessResult;
 import org.joget.workflow.model.service.WorkflowManager;
+import org.joget.workflow.model.service.WorkflowUserManager;
+import org.joget.workflow.util.WorkflowUtil;
+import org.json.JSONArray;
+import org.springframework.context.ApplicationContext;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
 
-public class StartProcessTool extends DefaultApplicationPlugin {
+public class StartProcessTool extends DefaultApplicationPlugin implements PluginWebSupport {
     @Override
     public String getName() {
         return AppPluginUtil.getMessage("startProcess.startProcessTool", getClassName(), "/messages/StartProcess");
@@ -34,7 +45,7 @@ public class StartProcessTool extends DefaultApplicationPlugin {
         AppDefinition appDefinition = (AppDefinition) map.get("appDef");
         WorkflowAssignment workflowAssignment = (WorkflowAssignment) map.get("workflowAssignment");
         WorkflowManager workflowManager = (WorkflowManager) AppUtil.getApplicationContext().getBean("workflowManager");
-        String processDefId = appDefinition.getAppId() + "#latest#" + map.get("processDefId");
+        String processDefId = map.get("appId") + "#latest#" + map.get("processId");
         Map<String, String> workflowVariables = Arrays.stream(((Object[]) map.get("workflowVariables")))
                 .map(o -> (Map<String, Object>)o)
                 .collect(HashMap::new, (m, o) -> m.put(o.get("variable").toString(), o.get("value").toString()), Map::putAll);
@@ -65,6 +76,75 @@ public class StartProcessTool extends DefaultApplicationPlugin {
 
     @Override
     public String getPropertyOptions() {
-        return AppUtil.readPluginResource(getClassName(), "/properties/StartProcessTool.json", null, false, "/messages/StartProcess");
+        return AppUtil.readPluginResource(getClassName(), "/properties/StartProcessTool.json", new String[] {getClassName(), getClassName()}, false, "/messages/StartProcess");
+    }
+
+    @Override
+    public void webService(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        boolean isAdmin = WorkflowUtil.isCurrentUserInRole(WorkflowUserManager.ROLE_ADMIN);
+        if (!isAdmin) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        ApplicationContext ac = AppUtil.getApplicationContext();
+        AppService appService = (AppService) ac.getBean("appService");
+        AppDefinitionDao appDefinitionDao = (AppDefinitionDao) ac.getBean("appDefinitionDao");
+        WorkflowManager workflowManager = (WorkflowManager) ac.getBean("workflowManager");
+
+        String action = request.getParameter("action");
+
+        if ("apps".equalsIgnoreCase(action)) {
+            try {
+                JSONArray jsonArray = new JSONArray();
+
+                Map<String, String> empty = new HashMap<>();
+                empty.put("value", "");
+                empty.put("label", "");
+                jsonArray.put(empty);
+
+                Collection<AppDefinition> appList = appDefinitionDao.findPublishedApps(null, null, null, null);
+
+                for (AppDefinition app : appList) {
+                    Map<String, String> option = new HashMap<String, String>();
+                    option.put("value", app.getAppId());
+                    option.put("label", app.getName() + " v"+app.getVersion()+" (" + app.getAppId() + ")");
+                    jsonArray.put(option);
+                }
+
+                jsonArray.write(response.getWriter());
+
+            } catch (Exception ex) {
+                LogUtil.error(this.getClass().getName(), ex, "Get Run Process's options Error!");
+            }
+        } else if ("processes".equalsIgnoreCase(action)) {
+            String appId = request.getParameter("appId");
+            String appVersion = String.valueOf(appDefinitionDao.getPublishedVersion(appId));
+            try {
+                JSONArray jsonArray = new JSONArray();
+                AppDefinition appDef = appService.getAppDefinition(appId, appVersion);
+                PackageDefinition packageDefinition = appDef.getPackageDefinition();
+                Long packageVersion = (packageDefinition != null) ? packageDefinition.getVersion() : new Long(1);
+                Collection<WorkflowProcess> processList = workflowManager.getProcessList(appId, packageVersion.toString());
+
+                Map<String, String> empty = new HashMap<>();
+                empty.put("value", "");
+                empty.put("label", "");
+                jsonArray.put(empty);
+
+                for (WorkflowProcess p : processList) {
+                    Map<String, String> option = new HashMap<String, String>();
+                    option.put("value", p.getIdWithoutVersion());
+                    option.put("label", p.getName() + " (" + p.getIdWithoutVersion() + ")");
+                    jsonArray.put(option);
+                }
+
+                jsonArray.write(response.getWriter());
+            } catch (Exception ex) {
+                LogUtil.error(this.getClass().getName(), ex, "Get Run Process's options Error!");
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+        }
     }
 }
